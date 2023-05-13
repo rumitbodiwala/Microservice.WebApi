ed612eaeeb8a41c3355643d5a7abd9a34caa92e1
Dhruvesh Panchal <Dhruvesh@calcuquote.com>
2023-05-13 10:59:42 +0530

    API Changes

--- Microservice.WebApi-master-master/Ordering.Microservice/Entities/Order.cs
+++ Microservice.WebApi-master-master/Ordering.Microservice/Entities/Order.cs
@@ -11,7 +11,7 @@ namespace Ordering.Microservice.Entities
         }
         public int Id { get; set; }
 
-        public DateTime OrderDate { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
 
         public string Username { get; set; }
 
